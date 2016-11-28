26c74955833960c106fd1d1cc43ddf6d04a49226
Andrew Schuster <aschuster93@gmail.com>
2016-11-28 00:25:14 +0000

    Update settings to use gunicorn

--- slack_bot_bot/settings.py
+++ slack_bot_bot/settings.py
@@ -111,6 +111,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "gunicorn",
 )
 
 # A sample logging configuration. The only tangible logging
